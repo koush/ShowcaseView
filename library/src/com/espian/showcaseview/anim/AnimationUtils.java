9aede90d2491e233545d24d55e9a2d0d9e534bdf
Koushik Dutta <koushd@gmail.com>
2014-03-02 22:50:12 -0800

    remove nine old androids

--- library/src/com/espian/showcaseview/anim/AnimationUtils.java
+++ library/src/com/espian/showcaseview/anim/AnimationUtils.java
@@ -1,12 +1,10 @@
 package com.espian.showcaseview.anim;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Handler;
 import android.view.View;
-
-import com.nineoldandroids.animation.Animator;
-import com.nineoldandroids.animation.AnimatorSet;
-import com.nineoldandroids.animation.ObjectAnimator;
-import com.nineoldandroids.view.ViewHelper;
 
 public class AnimationUtils {
 
@@ -28,15 +26,15 @@ public class AnimationUtils {
     }
 
     public static float getX(View view) {
-        return ViewHelper.getX(view);
+        return view.getX();
     }
 
     public static float getY(View view) {
-        return ViewHelper.getY(view);
+        return view.getY();
     }
 
     public static void hide(View view) {
-        ViewHelper.setAlpha(view, INVISIBLE);
+        view.setAlpha(INVISIBLE);
     }
 
     public static ObjectAnimator createFadeInAnimation(Object target, final AnimationStartListener listener) {
@@ -97,7 +95,7 @@ public class AnimationUtils {
                                                       float offsetStartX, float offsetStartY,
                                                       float offsetEndX, float offsetEndY,
                                                       final AnimationEndListener listener) {
-        ViewHelper.setAlpha(view, INVISIBLE);
+        view.setAlpha(INVISIBLE);
 
         ObjectAnimator alphaIn = ObjectAnimator.ofFloat(view, ALPHA, INVISIBLE, VISIBLE).setDuration(500);
 
